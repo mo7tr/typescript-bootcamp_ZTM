87485b8d7a01d8618433a58e3b258080904603b8
mo7tr <mo7tr@gmail.com>
2023-10-02 21:22:20 +0100

    weather app ui

--- app/weather-ui/src/backend/weatherapi.ts
+++ app/weather-ui/src/backend/weatherapi.ts
@@ -1,4 +1,5 @@
-import axios from "axios";
+import { z } from "zod";
+import type { AxiosStatic } from "axios";
 
 const weatherCodes: Record<number, string> = {
   0: "Clear sky",
@@ -31,74 +32,70 @@ const weatherCodes: Record<number, string> = {
   99: "Thunderstorm with heavy hail",
 };
 
-interface CurrentWeatherApiResponse {
-  temperature: number;
-  windspeed: number;
-  winddirection: number;
-  weathercode: number;
-  // day or night
-  is_day: number;
-  time: string;
-}
+export const currentWeatherApiResponseSchema = z.object({
+  current_weather: z.object({
+    temperature: z.number(),
+    windspeed: z.number(),
+    winddirection: z.number(),
+    weathercode: z.number(),
+    is_day: z.number(),
+    time: z.string(),
+  }),
+  hourly_units: z.object({
+    temperature_2m: z.string(),
+  }),
+  hourly: z.object({
+    temperature_2m: z.array(z.number()),
+  }),
+});
+
+export type CurrentWeatherApiResponse = z.infer<
+  typeof currentWeatherApiResponseSchema
+>;
 
 export interface Temperature {
   value: number;
   unit: string;
 }
-const formatTemperature = (temp: Temperature): string =>
-  `${temp.value}${temp.unit}`;
-
 export interface Wind {
   speed: number;
   direction: number;
   unit: string;
 }
 
-const formatWind = (wind: Wind): string => `${wind.speed}${wind.unit}`;
-
 export class CurrentWeather {
   temperature: Temperature;
-  wind: Wind;
   weathercode: number;
-  daytime: boolean;
+  is_day: boolean;
   time: string;
+  hourlyTemp: number[];
 
   constructor(apiResponse: CurrentWeatherApiResponse) {
     this.temperature = {
-      value: apiResponse.temperature,
-      unit: "C",
+      value: apiResponse.current_weather.temperature,
+      unit: apiResponse.hourly_units.temperature_2m,
     };
-    this.wind = {
-      speed: apiResponse.windspeed,
-      direction: apiResponse.winddirection,
-      unit: "kmh",
-    };
-    this.weathercode = apiResponse.weathercode;
-    this.daytime = apiResponse.is_day === 1;
-    this.time = apiResponse.time;
+    this.weathercode = apiResponse.current_weather.weathercode;
+    this.is_day = apiResponse.current_weather.is_day === 1;
+    this.time = apiResponse.current_weather.time;
+    this.hourlyTemp = apiResponse.hourly.temperature_2m;
+  }
+
+  lowTemp(): number {
+    return this.hourlyTemp.reduce((a, b) => Math.min(a, b));
+  }
+
+  highTemp(): number {
+    return this.hourlyTemp.reduce((a, b) => Math.max(a, b));
   }
 
   condition(): string {
     return weatherCodes[this.weathercode];
   }
-
-  format(): string {
-    const descriptionLen = 16;
-
-    const temp = "Temperature".padStart(descriptionLen, " ");
-    const windSpeed = "Wind Speed".padStart(descriptionLen, " ");
-    const condition = "Condition".padStart(descriptionLen, " ");
-
-    const formatted: string[] = [];
-    formatted.push(`${temp}: ${formatTemperature(this.temperature)}`);
-    formatted.push(`${windSpeed}: ${formatWind(this.wind)}`);
-    formatted.push(`${condition}: ${this.condition()}`);
-
-    return formatted.join("\n");
-  }
 }
 
 export async function fetchWeatherData(
+  axios: AxiosStatic,
   apiUrl: string,
   lat: string,
   lon: string
@@ -111,20 +108,19 @@ export async function fetchWeatherData(
       longitude: lon,
       hourly: "temperature_2m",
       temperature_unit: "celsius",
-      windspeed_unit: "kmh",
       current_weather: true,
+      forecast_days: 1,
     },
   };
 
   const response = await axios.request(options);
 
   if (response.status === 200) {
-    if (response.data?.current_weather !== undefined) {
-      // This might fail if the API changes. We'll see how to use Zod later to
-      // make this more type safe.
-      const res = response.data.current_weather as CurrentWeatherApiResponse;
+    try {
+      const res = currentWeatherApiResponseSchema.parse(response.data);
       return new CurrentWeather(res);
-    } else {
+    } catch (e) {
+      console.error(e);
       throw new Error("Received invalid API response");
     }
   } else {
