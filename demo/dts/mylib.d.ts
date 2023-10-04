b2b9adf0f2dd2a0e5f942fedbcbb6eaaf6f2850f
mo7tr <mo7tr@gmail.com>
2023-10-04 16:38:50 +0100

    custom iterator, dts

---
+++ demo/dts/mylib.d.ts
@@ -0,0 +1,12 @@
+export function add(a: number, b: number, ...numbers: number[]): number;
+export function max(arr: number[]): number | null;
+export function quote(message: string): () => string;
+
+const CaseKinds = ["lowercase", "uppercase"] as const;
+export type CaseKind = (typeof CaseKinds)[number];
+export function setCase(message: string, kind: CaseKind): string;
+
+// could also be:
+
+// export type CaseKind = "lowercase" | "uppercase";
+// export function setCase(message: string, kind: CaseKind): string;
