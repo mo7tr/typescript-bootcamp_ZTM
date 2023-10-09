556f0d990f4f9c3334900826ac3d6f579b393dd5
mo7tr <mo7tr@gmail.com>
2023-10-09 22:20:47 +0100

    last bootcamp project

---
+++ app/submit-form/src/shared/password-rules.ts
@@ -0,0 +1,8 @@
+export function checkComplexity(plainPassword: string): string[] {
+  const errors: string[] = [];
+  if (plainPassword.length < 8)
+    errors.push("Password must be at least 8 characters long.");
+  // more rules here;
+
+  return errors;
+}
